5282d9e95183d5bb9b9e8e7c32212c0fdd64cdd8
jakekasan <jake.kasan@gmail.com>
2025-05-15 08:20:10 +0100

    feat: add find folders

--- xdg_config/nvim/lua/telescope/find_folders.lua
+++ xdg_config/nvim/lua/telescope/find_folders.lua
@@ -14,36 +14,6 @@ M.find_folders = function(opts)
     { cwd = opts.cwd, entry_maker = make_entry.gen_from_string(opts) }
   )
 
-  -- local finder = finders.new_dynamic({
-  --   fn = function(prompt)
-  --     local job1 = vim.system({
-  --       "find",
-  --       ".",
-  --       "-type",
-  --       "d",
-  --     }, { cwd=opts.cwd }):wait()
-  --
-  --     local job2 = vim.system({
-  --       "sed",
-  --       [[ s/^\.\/// ]],
-  --     }, { stdin = job1.stdout or "" }):wait()
-  --
-  --     local job3 = vim.system({
-  --       "tail",
-  --       "-n",
-  --       "+2",
-  --     }, { stdin = job2.stdout or "" }):wait()
-  --
-  --     local job4 = vim.system({
-  --       "rg",
-  --       prompt
-  --     }, { stdin = job3.stdout or "" }):wait()
-  --
-  --     return vim.split(job4.stdout or "", "\n")
-  --   end,
-  --   entry_maker = make_entry.gen_from_string(opts)
-  -- })
-
   pickers.new(opts, {
     debounce = 100,
     prompt_title = "Find Folders",
@@ -55,7 +25,5 @@ end
 
 M.setup = function()
 end
-
-M.find_folders()
 
 return M
